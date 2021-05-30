408a557a979a531e62f23910d1c0fbe71aab71ad
User <nani2006ster@gmail.com>
2021-05-30 15:28:05 +0530

    initial commit

---
+++ process.py
@@ -0,0 +1,18 @@
+import sys
+from config import db_details
+from util import get_tables
+
+def process():
+    env=sys.argv[1]
+    db_detail=db_details[env]
+    tables=get_tables('tables_list')
+    for table in tables['table_name']:
+        print(table)
+
+
+
+
+
+
+if __name__=='__main__':
+    process()
